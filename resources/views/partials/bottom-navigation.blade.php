b14ee33dc1a999c3fbe34b118f838805c363a978
Dzikri Khasnudin <dzikri.khasnudin2@gmail.com>
2025-06-12 20:12:30 +0700

    memperbaiki font size pada bottom navigation

--- resources/views/partials/bottom-navigation.blade.php
+++ resources/views/partials/bottom-navigation.blade.php
@@ -7,7 +7,7 @@
                 <path
                     d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
             </svg>
-            <span class="text-sm">Home</span>
+            <span class="text-xs">Home</span>
         </x-bottom-nav-link>
         <x-bottom-nav-link href="{{ route('product.index') }}" :active="request()->routeIs('product.index')">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -15,7 +15,7 @@
                 <path
                     d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0" />
             </svg>
-            <span class="text-sm ">Product</span>
+            <span class="text-xs">Product</span>
         </x-bottom-nav-link>
         <x-bottom-nav-link href="{{ route('front.category') }}" :active="request()->routeIs('front.category')">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -25,7 +25,7 @@
                 <path
                     d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043z" />
             </svg>
-            <span class="text-sm">Category</span>
+            <span class="text-xs">Category</span>
         </x-bottom-nav-link>
         <x-bottom-nav-link href="{{ route('order.tracking') }}" :active="request()->routeIs('order.tracking')">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -33,7 +33,7 @@
                 <path fill-rule="evenodd"
                     d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0m-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z" />
             </svg>
-            <span class="text-sm">My Order</span>
+            <span class="text-xs">My Order</span>
         </x-bottom-nav-link>
     </div>
 </div>
